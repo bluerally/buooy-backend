2ae2cf1d7d21fed63778ef3a9eba01b3c539ee81
myeongjin yang <78643271+audwls624@users.noreply.github.com>
2023-12-12 17:44:55 +0900

    docker hub로 푸쉬하는 로직으로 변경(포트, compose 파일 변경)

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,18 @@
 echo "> 배포 시작" >> /home/ec2-user/deploy.log
 
 cd /home/ec2-user/app
-sudo docker-compose pull
-sudo docker-compose up --build -d
 
-# 이전 버전의 이미지 정리
-echo "> 이전 버전의 Docker 이미지 정리" >> /home/ec2-user/deploy.log
+# Pulling the latest image based on tag
+echo "> Pulling latest Docker images" >> /home/ec2-user/deploy.log
+IMAGE_TAG=$(git describe --tags --abbrev=0)
+sudo docker-compose pull app:$IMAGE_TAG
+
+# Starting services
+echo "> Starting Docker Compose services" >> /home/ec2-user/deploy.log
+sudo docker-compose up -d
+
+# Cleaning up old images
+echo "> Cleaning up old Docker images" >> /home/ec2-user/deploy.log
 sudo docker image prune -f
 
 echo "> 배포 완료" >> /home/ec2-user/deploy.log
