dbfea36d04bbb44799105dbdb62da7f754eee66f
jin <audwls624@gmail.com>
2023-12-13 01:57:33 +0900

    docker image git tag 반영 추가

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,7 @@ sudo docker pull bluerally/bluerally-be:$IMAGE_TAG
 
 sudo sed -i "s/image: bluerally\/bluerally-be:.*/image: bluerally\/bluerally-be:$IMAGE_TAG/" docker-compose.yml
 echo "$(date '+%Y-%m-%d %H:%M:%S') > Starting Docker Compose services" >> /home/ec2-user/deploy.log
+sudo docker-compose down
 sudo docker-compose up -d
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') > Cleaning up old Docker images" >> /home/ec2-user/deploy.log
