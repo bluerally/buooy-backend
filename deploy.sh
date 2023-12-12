0d3205e04ae5901fa3384abed013e646384b58e4
jin <audwls624@gmail.com>
2023-12-12 22:46:14 +0900

    deploy.sh에서  배포시, 최신 docker 이미지 받아오도록 수정

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,12 @@ echo "> 배포 시작" >> /home/ec2-user/deploy.log
 
 cd /home/ec2-user/app
 
-# Pulling the latest image based on tag
-echo "> Pulling latest Docker images" >> /home/ec2-user/deploy.log
-IMAGE_TAG=$(git describe --tags --abbrev=0)
-sudo docker-compose pull app:$IMAGE_TAG
+echo "> Pulling latest Docker image" >> /home/ec2-user/deploy.log
+sudo docker pull bluerally/bluerally-be:latest
 
-# Starting services
 echo "> Starting Docker Compose services" >> /home/ec2-user/deploy.log
 sudo docker-compose up -d
 
-# Cleaning up old images
 echo "> Cleaning up old Docker images" >> /home/ec2-user/deploy.log
 sudo docker image prune -f
 
