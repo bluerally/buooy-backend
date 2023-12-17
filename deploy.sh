364213ed3f8494d80d933544c2942b0ce3144554
jin <audwls624@gmail.com>
2023-12-18 00:13:20 +0900

    .env 파일 생성 스크립트 추가, 환경변수 전달 추가

--- deploy.sh
+++ deploy.sh
@@ -4,27 +4,19 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') > 배포 시작" >> /home/ec2-user/deploy.log
 
 cd /home/ec2-user/app
 
-# 환경 변수 가져오기
-DB_HOST=$(aws ssm get-parameter --name "/DB_HOST" --query "Parameter.Value" --output text)
-DB_PORT=$(aws ssm get-parameter --name "/DB_PORT" --query "Parameter.Value" --output text)
-DB_NAME=$(aws ssm get-parameter --name "/DB_NAME" --query "Parameter.Value" --output text)
-DB_PASSWORD=$(aws ssm get-parameter --name "/DB_PASSWORD" --query "Parameter.Value" --output text)
-DB_USER=$(aws ssm get-parameter --name "/DB_USER" --query "Parameter.Value" --output text)
-GOOGLE_CLIENT_ID=$(aws ssm get-parameter --name "/GOOGLE_CLIENT_ID" --query "Parameter.Value" --output text)
-GOOGLE_CLIENT_SECRET=$(aws ssm get-parameter --name "/GOOGLE_CLIENT_SECRET" --query "Parameter.Value" --output text)
-SECRET_KEY=$(aws ssm get-parameter --name "/SECRET_KEY" --query "Parameter.Value" --output text)
-APP_ENV=$(aws ssm get-parameter --name "/APP_ENV" --query "Parameter.Value" --output text)
+# .env 파일 생성 또는 초기화
+echo "" > .env
 
-# 환경변수를 Docker Compose 환경에 전달
-export DB_HOST
-export DB_PORT
-export DB_NAME
-export DB_PASSWORD
-export DB_USER
-export GOOGLE_CLIENT_ID
-export GOOGLE_CLIENT_SECRET
-export SECRET_KEY
-export APP_ENV
+# 환경 변수 가져오기 및 .env 파일에 기록
+echo "DB_HOST=$(aws ssm get-parameter --name "/DB_HOST" --query "Parameter.Value" --output text)" >> .env
+echo "DB_PORT=$(aws ssm get-parameter --name "/DB_PORT" --query "Parameter.Value" --output text)" >> .env
+echo "DB_NAME=$(aws ssm get-parameter --name "/DB_NAME" --query "Parameter.Value" --output text)" >> .env
+echo "DB_PASSWORD=$(aws ssm get-parameter --name "/DB_PASSWORD" --query "Parameter.Value" --output text)" >> .env
+echo "DB_USER=$(aws ssm get-parameter --name "/DB_USER" --query "Parameter.Value" --output text)" >> .env
+echo "GOOGLE_CLIENT_ID=$(aws ssm get-parameter --name "/GOOGLE_CLIENT_ID" --query "Parameter.Value" --output text)" >> .env
+echo "GOOGLE_CLIENT_SECRET=$(aws ssm get-parameter --name "/GOOGLE_CLIENT_SECRET" --query "Parameter.Value" --output text)" >> .env
+echo "SECRET_KEY=$(aws ssm get-parameter --name "/SECRET_KEY" --query "Parameter.Value" --output text)" >> .env
+echo "APP_ENV=$(aws ssm get-parameter --name "/APP_ENV" --query "Parameter.Value" --output text)" >> .env
 
 
 aws s3 cp s3://blue-rally/metadata.txt metadata.txt
