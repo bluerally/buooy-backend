6870a5046db3972d832205fa1dabc8d715d8554f
myeongjin yang <78643271+audwls624@users.noreply.github.com>
2024-01-23 23:43:10 +0900

    user api response 모델 수정, login callback api 예외처리 범위 수정

--- users/routers.py
+++ users/routers.py
@@ -1,10 +1,12 @@
 import logging
 import uuid
-from typing import List, Optional
+from typing import List, Optional, Any
 
 from fastapi import APIRouter, HTTPException, status, Depends, Request
 from fastapi.responses import RedirectResponse
 
+from common.cache_constants import CACHE_KEY_LOGIN_REDIRECT_UUID
+from common.cache_utils import RedisManager
 from common.choices import SocialAuthPlatform
 from common.config import LOGIN_REDIRECT_URL
 from common.constants import (
@@ -12,13 +14,10 @@ from common.constants import (
     AUTH_PLATFORM_KAKAO,
     AUTH_PLATFORM_NAVER,
 )
-from common.cache_constants import CACHE_KEY_LOGIN_REDIRECT_UUID
 from common.dependencies import get_current_user
-from common.dtos import BaseResponse
 from common.logging_configs import LoggingAPIRoute
 from users.auth import GoogleAuth, KakaoAuth, SocialLogin, NaverAuth
 from users.dtos import (
-    SocialLoginTokenResponse,
     UserInfo,
     SocialLoginRedirectResponse,
     RedirectUrlInfo,
@@ -39,7 +38,6 @@ from users.utils import (
     create_access_token,
     is_active_refresh_token,
 )
-from common.cache_utils import RedisManager
 
 user_router = APIRouter(
     prefix="/api/user",
@@ -94,73 +92,74 @@ async def social_auth_callback(
     error_description: Optional[str] = None,
 ) -> RedirectResponse:
     auth: SocialLogin
-
-    if platform == AUTH_PLATFORM_GOOGLE:
-        auth = GoogleAuth()
-    elif platform == AUTH_PLATFORM_KAKAO:
-        if error is not None or error_description is not None:
-            # TODO logging 필요
-            # return RedirectResponse(
-            #     url=f"{LOGIN_REDIRECT_URL}/login/{platform.value}?error={error}&error_decs={error_description}",
-            #     status_code=status.HTTP_406_NOT_ACCEPTABLE,
-            # )
-            logging.error(
-                f"[Auth Error]:platform-{platform.value} | error-{error} | desc-{error_description}"
-            )
-            raise HTTPException(
-                status_code=status.HTTP_406_NOT_ACCEPTABLE,
-                detail=f"Error: {error}, Error Detail:{error_description}",
-            )
-
-        # session_nonce = request.session.get("nonce")
-        # auth = KakaoAuth(session_nonce)
-        auth = KakaoAuth()
-    elif platform == AUTH_PLATFORM_NAVER:
-        if error is not None or error_description is not None:
-            # TODO logging 필요
-            # return RedirectResponse(
-            #     url=f"{LOGIN_REDIRECT_URL}/login/{platform.value}?error={error}&error_decs={error_description}",
-            #     status_code=status.HTTP_406_NOT_ACCEPTABLE,
-            # )
-            logging.error(
-                f"[Auth Error]:platform-{platform.value} | error-{error} | desc-{error_description}"
-            )
-            raise HTTPException(
-                status_code=status.HTTP_406_NOT_ACCEPTABLE,
-                detail=f"Error: {error}, Error Detail:{error_description}",
-            )
-
-        # session_state = request.session.get("state")
-        # auth = NaverAuth(session_state)
-        auth = NaverAuth()
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Unsupported platform"
-        )
-
-    user_info = await auth.get_user_data(code)
-    user = await User.get_or_none(sns_id=user_info.sns_id)
-    if not user:
-        user = await User.create(**user_info.__dict__)
-    else:
-        # 기존 사용자 정보 업데이트
-        if (
-            user.email != user_info.email
-            or user.profile_image != user_info.profile_image
-        ):
-            user.email = user_info.email
-            user.profile_image = user_info.profile_image
-            await user.save()
-
-    # uid 생성(인증한 유저 확인용)
     try:
-        r = RedisManager()
-        user_identify_uuid = str(uuid.uuid4())
-        cache_key = CACHE_KEY_LOGIN_REDIRECT_UUID.format(uuid=user_identify_uuid)
-        r.set_value(cache_key, user.id)
-        return RedirectResponse(
-            url=f"{LOGIN_REDIRECT_URL}/login/{platform.value}?uid={user_identify_uuid}"
-        )
+        if platform == AUTH_PLATFORM_GOOGLE:
+            auth = GoogleAuth()
+        elif platform == AUTH_PLATFORM_KAKAO:
+            if error is not None or error_description is not None:
+                # TODO logging 필요
+                # return RedirectResponse(
+                #     url=f"{LOGIN_REDIRECT_URL}/login/{platform.value}?error={error}&error_decs={error_description}",
+                #     status_code=status.HTTP_406_NOT_ACCEPTABLE,
+                # )
+                logging.error(
+                    f"[Auth Error]:platform-{platform.value} | error-{error} | desc-{error_description}"
+                )
+                raise HTTPException(
+                    status_code=status.HTTP_406_NOT_ACCEPTABLE,
+                    detail=f"Error: {error}, Error Detail:{error_description}",
+                )
+
+            # session_nonce = request.session.get("nonce")
+            # auth = KakaoAuth(session_nonce)
+            auth = KakaoAuth()
+        elif platform == AUTH_PLATFORM_NAVER:
+            if error is not None or error_description is not None:
+                # TODO logging 필요
+                # return RedirectResponse(
+                #     url=f"{LOGIN_REDIRECT_URL}/login/{platform.value}?error={error}&error_decs={error_description}",
+                #     status_code=status.HTTP_406_NOT_ACCEPTABLE,
+                # )
+                logging.error(
+                    f"[Auth Error]:platform-{platform.value} | error-{error} | desc-{error_description}"
+                )
+                raise HTTPException(
+                    status_code=status.HTTP_406_NOT_ACCEPTABLE,
+                    detail=f"Error: {error}, Error Detail:{error_description}",
+                )
+
+            # session_state = request.session.get("state")
+            # auth = NaverAuth(session_state)
+            auth = NaverAuth()
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="Unsupported platform"
+            )
+
+        is_new_user = True
+        user_info = await auth.get_user_data(code)
+        user = await User.get_or_none(sns_id=user_info.sns_id)
+        if not user:
+            user = await User.create(**user_info.__dict__)
+        else:
+            is_new_user = False
+            # 기존 사용자 정보 업데이트
+            if (
+                user.email != user_info.email
+                or user.profile_image != user_info.profile_image
+            ):
+                user.email = user_info.email
+                user.profile_image = user_info.profile_image
+                await user.save()
+
+            # uid 생성(인증한 유저 확인용)
+            r = RedisManager()
+            user_identify_uuid = str(uuid.uuid4())
+            cache_key = CACHE_KEY_LOGIN_REDIRECT_UUID.format(uuid=user_identify_uuid)
+            r.set_value(cache_key, [user.id, is_new_user])
+            return RedirectResponse(
+                url=f"{LOGIN_REDIRECT_URL}/login/{platform.value}?uid={user_identify_uuid}"
+            )
     except Exception as e:
         logging.error(e)
         raise HTTPException(
@@ -170,14 +169,14 @@ async def social_auth_callback(
 
 @user_router.post(
     "/auth/token",
-    response_model=SocialLoginTokenResponse,
+    response_model=LoginResponseData,
     status_code=status.HTTP_201_CREATED,
 )
-async def login_access_token(body: AccessTokenRequest) -> SocialLoginTokenResponse:
+async def login_access_token(body: AccessTokenRequest) -> LoginResponseData:
     user_uuid = body.user_uid
     r = RedisManager()
     cache_key = CACHE_KEY_LOGIN_REDIRECT_UUID.format(uuid=user_uuid)
-    user_id = r.get_value(cache_key)
+    user_id, is_new_user = r.get_value(cache_key)
     if not user_id:
         logging.error(f"[LOGIN API ERROR]: INVALID uuid: {user_uuid}")
         raise HTTPException(
@@ -201,24 +200,21 @@ async def login_access_token(body: AccessTokenRequest) -> SocialLoginTokenRespon
     # Access, Refresh 토큰 생성 및 저장
     access_token = create_access_token(data={"user_id": user.id})
     refresh_token = await create_refresh_token(user)
-    return SocialLoginTokenResponse(
-        message="Login successful",
-        data=LoginResponseData(
-            user_info=user_info,
-            access_token=access_token,
-            refresh_token=refresh_token,
-        ),
+    return LoginResponseData(
+        user_info=user_info,
+        access_token=access_token,
+        refresh_token=refresh_token,
     )
 
 
 @user_router.post(
     "/auth/token/refresh",
-    response_model=SocialLoginTokenResponse,
+    response_model=LoginResponseData,
     status_code=status.HTTP_201_CREATED,
 )
 async def access_token_refresh(
     body: RefreshTokenRequest, user: User = Depends(get_current_user)
-) -> SocialLoginTokenResponse:
+) -> LoginResponseData:
     refresh_token = body.refresh_token
 
     is_token_active = await is_active_refresh_token(
@@ -231,49 +227,37 @@ async def access_token_refresh(
 
     # 새로운 Access 토큰 생성
     access_token = create_access_token(data={"user_id": user.id})
-
-    return SocialLoginTokenResponse(
-        message="Token refreshed successfully",
-        data=LoginResponseData(
-            user_info=UserInfo(**user.__dict__),
-            access_token=access_token,
-        ),
+    return LoginResponseData(
+        user_info=UserInfo(**user.__dict__),
+        access_token=access_token,
     )
 
 
-@user_router.post(
-    "/auth/logout", response_model=BaseResponse, status_code=status.HTTP_200_OK
-)
-async def logout(user: User = Depends(get_current_user)) -> BaseResponse:
+@user_router.post("/auth/logout", response_model=None, status_code=status.HTTP_200_OK)
+async def logout(user: User = Depends(get_current_user)) -> str:
     # 사용자와 연관된 모든 리프레시 토큰을 비활성화
     await UserToken.filter(user=user, is_active=True).update(is_active=False)
 
-    return BaseResponse(message="Logout successful", data=None)
+    return "Logged out successfully"
 
 
 @user_router.get(
     "/certificates",
-    response_model=BaseResponse[List[CertificateName_Pydantic]],
+    response_model=List[CertificateName_Pydantic],
     status_code=status.HTTP_200_OK,
 )
-async def certificate_level_list() -> BaseResponse:
+async def certificate_level_list() -> Any:
     certificates = await CertificateName_Pydantic.from_queryset(Certificate.all())
-    return BaseResponse(
-        message="Certificates fetched successfully",
-        data=certificates,
-    )
+    return certificates
 
 
 @user_router.get(
     "/certificates/{certificate_id}/levels",
-    response_model=BaseResponse[List[CertificateLevel_Pydantic]],
+    response_model=List[CertificateLevel_Pydantic],
     status_code=status.HTTP_200_OK,
 )
-async def get_certificate_levels(certificate_id: int) -> BaseResponse:
+async def get_certificate_levels(certificate_id: int) -> Any:
     levels = await CertificateLevel_Pydantic.from_queryset(
         CertificateLevel.filter(certificate_id=certificate_id)
     )
-    return BaseResponse(
-        message="Certificate levels fetched successfully",
-        data=levels,
-    )
+    return levels
