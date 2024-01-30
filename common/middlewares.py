a9ae17c9aa9a666966143868800308cd52781fe9
myeongjin yang <78643271+audwls624@users.noreply.github.com>
2024-01-30 20:26:55 +0900

    auth middleware 예외처리 추가

--- common/middlewares.py
+++ common/middlewares.py
@@ -1,12 +1,15 @@
-from fastapi import Request, HTTPException
+from fastapi import Request, HTTPException, Response
 from starlette.middleware.base import BaseHTTPMiddleware
 from users.models import User
 from jwt import decode, PyJWTError
 from common.config import SECRET_KEY, ALGORITHM
+from typing import Callable, Awaitable
 
 
 class AuthMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
+    async def dispatch(
+        self, request: Request, call_next: Callable[[Request], Awaitable[Response]]
+    ) -> Response:
         token = request.headers.get("Authorization")
         request.state.user = None
         if token and token.startswith("Bearer "):
@@ -19,5 +22,7 @@ class AuthMiddleware(BaseHTTPMiddleware):
                 raise HTTPException(
                     status_code=403, detail="Could not validate credentials"
                 )
+            except Exception as e:
+                raise HTTPException(status_code=500, detail=str(e))
         response = await call_next(request)
         return response
