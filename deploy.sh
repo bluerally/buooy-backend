9ad58cd9f68236740f3b12740523c500669cd930
myeongjin yang <78643271+audwls624@users.noreply.github.com>
2024-03-24 20:09:32 +0900

    배포 로깅 수정

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') > 배포 시작" >> /home/ec2-user/deploy.log
+DEPLOY_LOG="/home/ec2-user/deploy.log"
+echo "$(date '+%Y-%m-%d %H:%M:%S') > 배포 시작" >> $DEPLOY_LOG
 
 cd /home/ec2-user/app
 
 # .env 파일 생성 또는 초기화
-echo "$(date '+%Y-%m-%d %H:%M:%S') > Initializing .env" >> /home/ec2-user/deploy.log
+echo "$(date '+%Y-%m-%d %H:%M:%S') > Initializing .env" >> $DEPLOY_LOG
 echo "" > .env
 
 # 환경 변수 가져오기 및 .env 파일에 기록
-echo "$(date '+%Y-%m-%d %H:%M:%S') > Writing Environmental variables into .env file" >> /home/ec2-user/deploy.log
+echo "$(date '+%Y-%m-%d %H:%M:%S') > Writing Environmental variables into .env file" >> $DEPLOY_LOG
 echo "DB_HOST=$(aws ssm get-parameter --name "/DB_HOST" --query "Parameter.Value" --output text)" >> .env
 echo "DB_PORT=$(aws ssm get-parameter --name "/DB_PORT" --query "Parameter.Value" --output text)" >> .env
 echo "DB_NAME=$(aws ssm get-parameter --name "/DB_NAME" --query "Parameter.Value" --output text)" >> .env
@@ -32,26 +33,20 @@ echo "S3_SECRET_KEY=$(aws ssm get-parameter --name "/S3_SECRET_KEY" --query "Par
 aws s3 cp s3://blue-rally/metadata.txt metadata.txt
 IMAGE_TAG=$(cat metadata.txt)
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') > Pulling latest Docker image with tag: $IMAGE_TAG" >> /home/ec2-user/deploy.log
-sudo docker pull bluerally/bluerally-be:$IMAGE_TAG
+# Docker 이미지 다운로드 및 서비스 시작 로깅
+echo "$(date '+%Y-%m-%d %H:%M:%S') > Pulling Docker image with tag: $IMAGE_TAG" >> $DEPLOY_LOG
+sudo docker pull bluerally/bluerally-be:$IMAGE_TAG 2>&1 | tee -a $DEPLOY_LOG
 
-sudo sed -i "s/image: bluerally\/bluerally-be:.*/image: bluerally\/bluerally-be:$IMAGE_TAG/" docker-compose.yml
-echo "$(date '+%Y-%m-%d %H:%M:%S') > Starting Docker Compose services" >> /home/ec2-user/deploy.log
-sudo docker-compose down
+echo "$(date '+%Y-%m-%d %H:%M:%S') > Starting Docker Compose services" >> $DEPLOY_LOG
+sudo docker-compose down 2>&1 | tee -a $DEPLOY_LOG
+sudo docker-compose up -d 2>&1 | tee -a $DEPLOY_LOG
 
-# Aerich 마이그레이션 실행
-echo "$(date '+%Y-%m-%d %H:%M:%S') > Running Aerich database migrations" >> /home/ec2-user/deploy.log
-sudo docker-compose run --rm app aerich upgrade
+# Docker Compose를 사용하여 Aerich 마이그레이션 실행
+echo "$(date '+%Y-%m-%d %H:%M:%S') > Running Aerich database migrations" >> $DEPLOY_LOG
+sudo docker-compose run --rm app aerich upgrade 2>&1 | tee -a $DEPLOY_LOG
 
-sudo docker-compose up -d
+# 오래된 Docker 이미지 정리
+echo "$(date '+%Y-%m-%d %H:%M:%S') > Cleaning up old Docker images" >> $DEPLOY_LOG
+sudo docker image prune -a -f 2>&1 | tee -a $DEPLOY_LOG
 
-#sleep 10
-
-# Running Aerich database migrations(docker-compose run으로 migration 안될시 사용)
-#echo "$(date '+%Y-%m-%d %H:%M:%S') > Running Aerich database migrations" >> /home/ec2-user/deploy.log
-#sudo docker-compose exec app aerich upgrade
-
-echo "$(date '+%Y-%m-%d %H:%M:%S') > Cleaning up old Docker images" >> /home/ec2-user/deploy.log
-sudo docker image prune -a -f
-
-echo "$(date '+%Y-%m-%d %H:%M:%S') > 배포 완료" >> /home/ec2-user/deploy.log
+echo "$(date '+%Y-%m-%d %H:%M:%S') > 배포 완료" >> $DEPLOY_LOG
