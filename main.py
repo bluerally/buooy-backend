0b6252b2eb20a75401998978bebafb844a81a838
myeongjin yang <78643271+audwls624@users.noreply.github.com>
2024-09-26 18:50:59 +0800

    마파티 마감 스케쥴러 임시 주석 처리

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ from parties.routers import party_router
 from users.routers import user_router
 from common.logging_configs import LoggingAPIRoute
 from feedback.routers import feedback_router
-from common.scheduler import start_scheduler, scheduler
+from common.scheduler import scheduler
 
 
 @asynccontextmanager
 async def lifespan(application: FastAPI) -> AsyncIterator[None]:
     await Tortoise.init(config=TORTOISE_ORM, timezone="Asia/Seoul")
-    start_scheduler()
+    # start_scheduler()
     yield
     scheduler.shutdown()
     await Tortoise.close_connections()
